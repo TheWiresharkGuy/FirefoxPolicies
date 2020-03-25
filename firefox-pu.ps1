a0429ccda33cf157bf6e42c02f5ceb39bf93a592
TheWiresharkGuy <TheWiresharkGuy@tuta.io>
2020-03-25 12:30:54 +0100

    Remove Desktop icon
    for Firefox Developer Edition

--- firefox-pu.ps1
+++ firefox-pu.ps1
@@ -1,2 +1,3 @@
 Copy-Item -Recurse -Path .\distribution -Destination "C:\Program Files\Mozilla Firefox\" -Verbose -Force
 Copy-Item -Recurse -Path .\distribution -Destination "C:\Program Files\Firefox Developer Edition\" -Verbose -Force
+Remove-Item -Verbose -Force "C:\Users\Public\Desktop\Firefox Developer Edition.lnk"
